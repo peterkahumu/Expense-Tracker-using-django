2067fabf0cdf7fe538960795f23b4ea3274da3a1
peterkahumu <muhumukip@gmail.com>
2024-09-15 14:06:52 +0300

    configured username validation and the registration form.

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,8 @@
 from .views import *
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('register', RegistrationView.as_view(), name='Register'),
+    path('validate-username', csrf_exempt(usernameValidation.as_view()), name = 'validate-username')
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.views import View
+import json
+from django.http import JsonResponse
 
 # Create your views here.
+class usernameValidation(View):
+    def post(self, request):
+       data = json.loads(request.body)
+       username = data['username']
+       
+       # check if username is valid
+       if not str(username).isalnum():
+           return JsonResponse({'username_error': 'Username should only contain alphanumeric characters'})
+       return JsonResponse ({'username_valid': True})
+       
+
 class RegistrationView(View):
     def get(self, request):
         return render(request, 'authentication/register.html')
