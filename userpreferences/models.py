8113de1139b96c65b02990f101d19930290d10f4
peterkahumu <muhumukip@gmail.com>
2024-09-18 12:21:36 +0300

    Started working on userpreferences page.

---
+++ userpreferences/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class UserPreferences(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
+    currency = models.CharField(max_length = 255, null = False, blank=False, default='KSH') 
+    
+    def __str__(self):
+        return f'{self.user.username}\'s preferences'
