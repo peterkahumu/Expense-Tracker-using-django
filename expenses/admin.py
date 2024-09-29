07b84471eaa31f00828f8f787f00b8b3885ada62
peterkahumu <muhumukip@gmail.com>
2024-09-29 10:05:29 +0300

    Added comments on expenses/admin.y

--- expenses/admin.py
+++ expenses/admin.py
@@ -3,9 +3,9 @@ from .models import *
 
 # Register your models here.
 class ExpenseAdmin(admin.ModelAdmin):
-    list_display = ['amount', 'description', 'date', 'category', 'owner']
-    search_fields = ['amount', 'description', 'date', 'category']
+    list_display = ['amount', 'description', 'date', 'category', 'owner'] # fields to display in the admin page.
     
-    list_per_page = 5
+    search_fields = ['amount', 'description', 'date', 'category']    # field to refer to during the search
+    list_per_page = 5 # paginantion
 admin.site.register(Expense, ExpenseAdmin)
 admin.site.register(Category)
