d58f7a16d780220957fcf71a623c5ea1f521a1e3
peterkahumu <muhumukip@gmail.com>
2024-09-29 10:01:07 +0300

    Updating the admin page.

--- expenses/admin.py
+++ expenses/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ['amount', 'description', 'date', 'category', 'owner']
+    search_fields = ['amount', 'description', 'date', 'category']
+    
+    list_per_page = 5
+admin.site.register(Expense, ExpenseAdmin)
 admin.site.register(Category)
